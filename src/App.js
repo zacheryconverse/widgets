b1a8ff4d7ea1104ee619775b47684e54a5d855a1
Zachery Converse <zacheryconverse@gmail.com>
2020-11-16 20:26:40 -0700

    render wiki api data. Bug in res

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import Accordion from './components/Accordion';
+import React from "react";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'A front end JS library.'
-  },
-  {
-    title: 'Why use React?',
-    content: 'It is a favorite among developers.'
-  },
-  {
-    title: 'How do you use React?',
-    content: 'By creating components.'
-  }
-]
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "A front end JS library.",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "It is a favorite among developers.",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "By creating components.",
+//   },
+// ];
 
-export default () => {
+const App = () => {
   return (
-    <h1>
-      <Accordion items={items} />
-    </h1>
-  )
+    <div>
+      <Search />
+    </div>
+  );
 };
+
+export default App;
