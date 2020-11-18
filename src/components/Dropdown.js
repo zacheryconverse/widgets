b2a9aea1cd7eeee973c0faed828e6706b9f93e91
Zachery Converse <zacheryconverse@gmail.com>
2020-11-18 14:24:24 -0700

    ternary classname

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
-  const renderedOptions = options.map(option => {
+  const [open, setOpen] = useState(false);
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) return null;
+
     return (
-      <div key={option.value} className='item'>
+      <div
+        key={option.value}
+        onClick={() => onSelectedChange(option)}
+        className="item"
+      >
         {option.label}
-       </div>
+      </div>
     );
   });
 
-
   return (
-    <div className='ui form'>
-      <div className='field'>
-        <label className='label'>Select a Color</label>
-        <div className='ui selection dropdown visible active'>
-          <i className='dropdown icon'></i>
-          <div className='text'>Select Color</div>
-          <div className='menu visible transition'>
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
             {renderedOptions}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Dropdown;
